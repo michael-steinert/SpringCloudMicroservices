23d931e94012b5a6d26b1d9de3e2d5ecce04cf8b
Michael Steinert <Michael.Steinert.94@gmail.com>
2022-01-17 14:53:19 +0100

    Added Kubernetes Configuration

--- clients/src/main/java/com/company/clients/fraud/IFraudClient.java
+++ clients/src/main/java/com/company/clients/fraud/IFraudClient.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
-        name = "fraud"
+        name = "fraud",
+        url = "${clients.fraud.url}"
 )
 public interface IFraudClient {
     @GetMapping(path = "api/v1/fraud-check/{customerId}")
